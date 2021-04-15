1e7fb06a7377f42f1bbd4fd6ef4a3da33fde2769
alicex18 <>
2021-04-15 02:01:14 +0000

    I added an int() in line 10 to show that the computer should expect a number. There was a missing parenthesis at the end of line 10. Furthermore, I changed the "Number2" in line 13 to "number2", as well as adding parenthesis around the whole thing.  I added quotation marks around the text in line 14, and used * to show multiplication rather than parenthesis.

--- part3.py
+++ part3.py
@@ -8,7 +8,7 @@ There are (at least) 6 errors in this code. Fix them so that it runs properly.
 
 #code starts here
 number1 = int(input("Enter a number: "))
-number2 = int(input("Enter another number: "))
+number2 = int(input("Enter another number: ")) 
 
 print("The sum of your numbers is", number1 + number2)
 print("Seven times your second number is", 7 * number2)
